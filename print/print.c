dac5df94aa1b7d09a9678ff0cb379bfdb62215b1
Betül Öztürk <86080047+betuloztrk@users.noreply.github.com>
2022-01-27 21:00:03 +0300

    Add files via upload

---
+++ print/print.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+void print_hex(int value)
+{
+	printf("Value = %08X\n", value);
+}
+
+void print_dec(int value)
+{
+	printf("Value = %d\n", value);
+}
+
+
+
+
---
+++ print/print.h
@@ -0,0 +1,16 @@
+/*
+ * print.h
+ *
+ *  Created on: Oct 18, 2021
+ *      Author: user
+ */
+
+#ifndef PRINT_H_
+#define PRINT_H_
+
+
+extern void print_hex(int);
+extern void print_dec(int);
+
+
+#endif /* PRINT_H_ */
